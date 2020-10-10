b2cc26eaefaf5d7c10ab2b58288c24cc532ced31
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-10-10 23:20:48 +1300

    Added more styling

--- movie/auth/auth.py
+++ movie/auth/auth.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import Blueprint, render_template, redirect, url_for, session
+from flask import Blueprint, render_template, redirect, url_for, session, request
 from flask_wtf import FlaskForm
 from password_validator import PasswordValidator
 from wtforms import StringField, PasswordField, SubmitField
@@ -26,7 +26,7 @@ def register():
             add_user(repo, form.username.data, form.password.data)
 
             # All is well, redirect the user to the login page.
-            return redirect(url_for('auth_bp.login'), 303)
+            return redirect(url_for('auth_bp.login', success=True))
         except NameNotUniqueException:
             username_error_message = 'Username taken. Please supply another'
 
@@ -46,6 +46,7 @@ def login():
     form = LoginForm()
     username_error_message = None
     password_error_message = None
+    is_register_success = bool(request.args.get('success'))
 
     if form.validate_on_submit():
         # Successful POST, i.e. the username and password have passed validation checking.
@@ -59,15 +60,15 @@ def login():
             # Initialise session and redirect the user to the home page.
             session.clear()
             session['username'] = user.user_name
-            return redirect(url_for('home_bp.home'), 303)
+            return redirect(url_for('home_bp.home'))
 
         except UnknownUserException:
             # Username not known to the system, set a suitable error message.
-            username_error_message = 'Username not recognised - please supply another'
+            username_error_message = 'Unrecognized username - please check and try again.'
 
         except AuthenticationException:
             # Authentication failed, set a suitable error message.
-            password_error_message = 'Password does not match supplied username - please check and try again'
+            password_error_message = 'Incorrect password - please check and try again.'
 
     # For a GET or a failed POST, return the Login Web page.
     return render_template(
@@ -75,6 +76,7 @@ def login():
         title='Log in',
         username_error_message=username_error_message,
         password_error_message=password_error_message,
+        is_register_success=is_register_success,
         form=form
     )
 
@@ -89,7 +91,7 @@ def login_required(view):
     @wraps(view)
     def wrapped_view(**kwargs):
         if 'username' not in session:
-            return redirect(url_for('auth_bp.login'), 303)
+            return redirect(url_for('auth_bp.login'))
         return view(**kwargs)
 
     return wrapped_view

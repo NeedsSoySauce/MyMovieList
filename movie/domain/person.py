97a2e12da76ae5f1dccb4dff3c3e96b8e7b101b5
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-10-22 18:54:44 +1300

    Change name mangled field to internal use

--- movie/domain/person.py
+++ movie/domain/person.py
@@ -5,28 +5,28 @@ class Person:
 
     @property
     def _full_name(self):
-        return self.__full_name
+        return self._person_full_name
 
     @_full_name.setter
     def _full_name(self, full_name: str) -> None:
         if full_name == "" or not isinstance(full_name, str):
-            self.__full_name = None
+            self._person_full_name = None
         else:
-            self.__full_name = full_name.strip()
+            self._person_full_name = full_name.strip()
 
     def __repr__(self) -> str:
-        return f"<{type(self).__name__} {self.__full_name}>"
+        return f"<{type(self).__name__} {self._person_full_name}>"
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, Person):
             return False
-        return other.__full_name == self.__full_name
+        return other._person_full_name == self._person_full_name
 
     def __lt__(self, other) -> bool:
         if not isinstance(other, Person):
             raise TypeError(
                 f"'<' not supported between instances of '{type(self).__name__}' and '{type(other).__name__}'")
-        return self.__full_name < other.__full_name
+        return self._person_full_name < other._person_full_name
 
     def __hash__(self) -> int:
-        return hash(self.__full_name)
+        return hash(self._person_full_name)

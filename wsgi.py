b15396269cd0566e62d67e44d2ee2a36d95bcaf7
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-10-29 08:19:55 +1300

    Move cache init into create_app

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,6 @@ from movie import create_app
 from cache import cache
 
 app = create_app()
-cache.init_app(app)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, threaded=False)
